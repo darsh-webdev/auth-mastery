16a93e4a801751a7ebfa7be608a17e7b071beeed
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-10-19 19:13:26 +0530

    feat: Implement sign-in functionality

--- src/app/auth/login/_components/sign-in-tab.tsx
+++ src/app/auth/login/_components/sign-in-tab.tsx
@@ -1,3 +1,93 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoadingSwap } from "@/components/ui/loading-swap";
+import { PasswordInput } from "@/components/ui/password-input";
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+const signInSchema = z.object({
+  email: z.email().min(1, "Email is required"),
+  password: z.string().min(8, "Password is required"),
+});
+
+type SignInForm = z.infer<typeof signInSchema>;
+
 export function SignInTab() {
-  return null;
+  const router = useRouter();
+
+  const form = useForm<SignInForm>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { isSubmitting } = form.formState;
+
+  const handleSignIn = async (data: SignInForm) => {
+    await authClient.signIn.email(
+      { ...data, callbackURL: "/" },
+      {
+        onError: (error) => {
+          toast.error(error.error.message || "Failed to sign in");
+        },
+        onSuccess: () => {
+          toast.success("Signed In successfully");
+          router.push("/");
+        },
+      }
+    );
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(handleSignIn)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your email" {...field} type="email" />
+              </FormControl>
+              <FormMessage></FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="Enter your password" {...field} />
+              </FormControl>
+              <FormMessage></FormMessage>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" disabled={isSubmitting} className="w-full">
+          <LoadingSwap isLoading={isSubmitting}>Sign In</LoadingSwap>
+        </Button>
+      </form>
+    </Form>
+  );
 }
