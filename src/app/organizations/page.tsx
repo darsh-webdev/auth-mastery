88d64557e7f60a9a204c6d7d87f8059a9c72a41a
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-11-21 20:27:56 +0530

    feat: Add create organization button and update page layout

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,7 +1,28 @@
-export default function OrganizationsPage() {
+import { auth } from "@/lib/auth/auth";
+import { ArrowLeft } from "lucide-react";
+import { headers } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import OrganizationSelect from "./_components/organization-select";
+import CreateOrganizationButton from "./_components/create-organiztion-button";
+import OrganizationTabs from "./_components/organization-tabs";
+
+export default async function OrganizationsPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (session === null) return redirect("/auth/login");
   return (
-    <div>
-      <h1>Organizations</h1>
+    <div className="container mx-auto my-6 px-4">
+      <Link href="/" className="inline-flex items-center mb-6">
+        <ArrowLeft className="size-4 mr-2" />
+        Back to home
+      </Link>
+
+      <div className="flex items-center mb-8 gap-2">
+        <OrganizationSelect />
+        <CreateOrganizationButton />
+      </div>
+
+      <OrganizationTabs />
     </div>
   );
 }
