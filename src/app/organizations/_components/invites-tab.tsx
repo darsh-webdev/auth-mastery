c0c69f51b287fdcb03e0ee758b20d4076c3baf66
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-11-26 21:21:42 +0530

    feat: Add pending invites table to InvitesTab

--- src/app/organizations/_components/invites-tab.tsx
+++ src/app/organizations/_components/invites-tab.tsx
@@ -1,3 +1,71 @@
+import { BetterAuthActionButton } from "@/components/auth/better-auth-action-button";
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { authClient } from "@/lib/auth/auth-client";
+
 export default function InvitesTab() {
-  return <div>Invites</div>;
+  const { data: activeOrganization } = authClient.useActiveOrganization();
+  const pendingInvites = activeOrganization?.invitations.filter(
+    (invite) => invite.status === "pending"
+  );
+
+  const cancelInvitation = (invitationId: string) => {
+    return authClient.organization.cancelInvitation({
+      invitationId,
+    });
+  };
+
+  return (
+    <div className="space-y-4">
+      <div className="flex justify-end">{/* <CreateInviteButton /> */}</div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Email</TableHead>
+            <TableHead>Roles</TableHead>
+            <TableHead>Expires</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {pendingInvites?.length === 0 ? (
+            <TableRow>
+              <TableCell colSpan={4} className="text-center text-md">
+                No invites found.
+              </TableCell>
+            </TableRow>
+          ) : (
+            pendingInvites?.map((invite) => (
+              <TableRow key={invite.id}>
+                <TableCell>{invite.email}</TableCell>
+                <TableCell>
+                  <Badge variant="outline">{invite.role}</Badge>
+                </TableCell>
+                <TableCell>
+                  {new Date(invite.expiresAt).toLocaleDateString()}
+                </TableCell>
+                <TableCell>
+                  <BetterAuthActionButton
+                    requireAreYouSure
+                    variant="destructive"
+                    size="sm"
+                    action={() => cancelInvitation(invite.id)}
+                  >
+                    Cancel
+                  </BetterAuthActionButton>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
